4d2d9110ac42615934efd3d56666208d4d5277fa
ilham bintang <ilham@paratekno.com>
2024-10-25 15:25:50 +0700

    env

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,6 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ubvyegfijthwscdmsugr.supabase.co'
+const supabaseUrl = process.env.APP_SUPABASE_URL || ""
 const supabaseKey = process.env.APP_SUPABASE_KEY || ""
 export const supabase = createClient(supabaseUrl, supabaseKey)
